01ef09110ac0f26ced3ecac372d7db089f2d30dd
Henrik Nordstrom <henrik@henriknordstrom.net>
2010-11-18 11:29:43 +0100

    Move recorder functionality to a new lib recorder.py and cookie support
    The cookie support is an automatic filter ignoring cookies for which
    there has been no set-cookie in the session.

--- libmproxy/playback.py
+++ libmproxy/playback.py
@@ -1,22 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2010  Henrik Nordstrom <henrik@henriknordstrom.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import controller
-import hashlib
 import utils
 import proxy
-
-class Count(dict):
-    def __init__(self):
-	dict.__init__(self)
-
-    def __missing__(self, key):
-	return 0
+import recorder
 
 class RecordMaster(controller.Master):
     """
         A simple master that just records to files.
     """
     def __init__(self, server, verbosity):
-	self.sequence = Count()
+        self.store = recorder.Recorder()
         self.verbosity = verbosity
         controller.Master.__init__(self, server)
 
@@ -27,43 +37,23 @@ class RecordMaster(controller.Master):
             self.shutdown()
 
     def handle_request(self, msg):
-	request = msg
-	headers = request.headers
-	utils.try_del(headers, 'if-modified-since')
-	utils.try_del(headers, 'if-none-match')
-	req_text = request.assemble()
-	m = hashlib.sha224(req_text).hexdigest()
-	path = (request.host + request.path)[:60].replace("/","_")+"."+m
-	n = self.sequence[path]
-	self.sequence[path] = n + 1
-	path = path + "." + str(n)
-	try:
-	    fp = open(path+".resp", 'r')
-	    proto, code, status = fp.readline().strip().split(" ", 2)
-	    code = int(code)
-	    headers = utils.Headers()
-	    headers.read(fp)
-	    utils.try_del(headers, 'transfer-encoding')
-	    if request.method == "HEAD":
-		content = None
-	    else:
-		content = proxy.read_http_body(fp, headers, True)
-	    fp.close()
-	    msg.ack(proxy.Response(request, code, proto, status, headers, content))
-	except IOError:
-	    print >> sys.stderr, ">>",
-	    print >> sys.stderr, request.short()
-	    print >> sys.stderr, "<<",
-	    print >> sys.stderr, "ERROR: No matching response: " + path
-	    print >> sys.stderr, req_text
-	    msg.kill = True
-	    msg.ack()
+        request = msg
+        try:
+            msg.ack(self.store.get_response(request))
+        except IOError:
+            print >> sys.stderr, ">>",
+            print >> sys.stderr, request.short()
+            print >> sys.stderr, "<<",
+            print >> sys.stderr, "ERROR: No matching response"
+            print >> sys.stderr, request.assemble()
+            msg.kill = True
+            msg.ack()
 
     def handle_response(self, msg):
-	request = msg.request
-	response = msg
-	print >> sys.stderr, ">>",
-	print >> sys.stderr, request.short()
-	print >> sys.stderr, "<<",
-	print >> sys.stderr, response.short()
+        request = msg.request
+        response = msg
+        print >> sys.stderr, ">>",
+        print >> sys.stderr, request.short()
+        print >> sys.stderr, "<<",
+        print >> sys.stderr, response.short()
         msg.ack()
--- libmproxy/record.py
+++ libmproxy/record.py
@@ -1,21 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2010  Henrik Nordstrom <henrik@henriknordstrom.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import controller
-import hashlib
 import utils
+import recorder
 
 class Count(dict):
     def __init__(self):
-	dict.__init__(self)
+        dict.__init__(self)
 
     def __missing__(self, key):
-	return 0
+        return 0
 
 class RecordMaster(controller.Master):
     """
         A simple master that just records to files.
     """
     def __init__(self, server, verbosity):
-	self.sequence = Count()
+        self.store = recorder.Recorder()
         self.verbosity = verbosity
         controller.Master.__init__(self, server)
 
@@ -26,28 +43,12 @@ class RecordMaster(controller.Master):
             self.shutdown()
 
     def handle_request(self, msg):
-	request = msg
-	headers = request.headers
-	utils.try_del(headers, 'if-modified-since')
-	utils.try_del(headers, 'if-none-match')
-	msg.ack()
+        msg.ack(self.store.filter_request(msg))
 
     def handle_response(self, msg):
-	print >> sys.stderr, ">>",
-	print >> sys.stderr, msg.request.short()
-	print >> sys.stderr, "<<",
-	print >> sys.stderr, msg.short()
-	req_text = msg.request.assemble()
-	resp_text = msg.assemble()
-	m = hashlib.sha224(req_text).hexdigest()
-	path = (msg.request.host + msg.request.path)[:60].replace("/","_")+"."+m
-	n = self.sequence[path]
-	self.sequence[path] = n + 1
-	path = path + "." + str(n)
-	f = open(path+".req", 'w')
-	f.write(req_text)
-	f.close()
-	f = open(path+".resp", 'w')
-	f.write(resp_text)
-	f.close()
+        print >> sys.stderr, ">>",
+        print >> sys.stderr, msg.request.short()
+        print >> sys.stderr, "<<",
+        print >> sys.stderr, msg.short()
+        self.store.save_response(msg)
         msg.ack()
